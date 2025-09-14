f20b35e39fecb290ece8d5beb2c65eba50864b60
Syed saif Sikander <sikandersyedsaif@gmail.com>
2025-09-14 14:17:38 +0000

    can you see the reason why my images are not loading still  what is the

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -1,4 +1,4 @@
-import data from './placeholder-images.json';
+import { placeholderImages } from './placeholder-images.json';
 
 export type ImagePlaceholder = {
   id: string;
@@ -7,4 +7,4 @@ export type ImagePlaceholder = {
   imageHint: string;
 };
 
-export const PlaceHolderImages: ImagePlaceholder[] = data.placeholderImages;
+export const PlaceHolderImages: ImagePlaceholder[] = placeholderImages;
