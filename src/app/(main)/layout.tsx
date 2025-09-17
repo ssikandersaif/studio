9cd445e1521f196ab3ceae2660c096d9e1d12557
Syed saif Sikander <sikandersyedsaif@gmail.com>
2025-09-17 07:49:32 +0000

    make it little small  (_for element <Sprout>_)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -34,7 +34,7 @@ export default function MainLayout({
       <Sidebar collapsible="icon" variant="sidebar">
         <SidebarHeader>
            <div className="flex items-center gap-3 p-2">
-            <Sprout className="h-24 w-24 text-primary" />
+            <Sprout className="h-16 w-16 text-primary" />
             <span className="min-w-0 flex-1 overflow-hidden whitespace-nowrap transition-all group-data-[state=collapsed]:w-0 group-data-[state=collapsed]:opacity-0 group-data-[state=collapsed]:delay-0 delay-200 text-4xl font-bold text-primary font-headline">
               Krishi Mitra
             </span>
